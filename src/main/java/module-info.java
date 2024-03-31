44011ed9a0fabbba45b46b009139a88670234c11
Kimral <semaytes2012@mail.ru>
2024-03-31 13:06:18 +0500

    Init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.usue.svetlyakov.coursework {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.usue.svetlyakov.coursework to javafx.fxml;
+    exports com.usue.svetlyakov.coursework;
+}
