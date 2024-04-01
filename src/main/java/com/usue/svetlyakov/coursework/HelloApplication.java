e92ed6a98c15c1953da5195841617ba957d23854
Kimral <semaytes2012@mail.ru>
2024-04-01 22:35:25 +0500

    Начало проработки GUI

--- src/main/java/com/usue/svetlyakov/coursework/HelloApplication.java
+++ src/main/java/com/usue/svetlyakov/coursework/HelloApplication.java
@@ -9,18 +9,31 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
+    private static final double min_window_width = 650;
+    private static final double min_window_height = 450;
     static public int multiply(int a, int b) {
         return a * b;
     }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = LoadScene("hello-view.fxml", min_window_width, min_window_height);
+        SetScene(stage, scene);
+
     }
+
+    private void SetScene(Stage stage, Scene scene) {
+        stage.setScene(scene);
+        stage.setTitle("Hello!");
+        stage.show();
+        stage.setMinWidth(scene.getWidth());
+        stage.setMinHeight(scene.getHeight());
+    }
+
+    private Scene LoadScene(String scene_fxml_path, double width, double height) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(scene_fxml_path));
+        return new Scene(fxmlLoader.load(), width, height);
+    }
+
 
     public static void main(String[] args) {
         launch();
